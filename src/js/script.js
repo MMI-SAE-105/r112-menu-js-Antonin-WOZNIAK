8a8baf0c963f7a7182466d39abee3c56abdc5e5f
Antonin-WOZNIAK <antonin.wozniak@edu.univ-fcomte.fr>
2024-11-15 16:34:46 +0100

    classe `noscroll`

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,12 @@
 const toggle = document.querySelector("button.menu-btn");
 const nav = document.querySelector("nav.menu");
+const body = document.body; 
 toggle.addEventListener("click", () => {   
     const isOpen = toggle.ariaExpanded === "true";
     const isClosed = !isOpen;
     console.log("isOpen : ", isOpen, "isClosed : ", isClosed);
     nav.ariaHidden = isOpen ? "true" : "false";
     toggle.ariaExpanded = isOpen ? "false" : "true";
+    body.classList.toggle("noscroll", !isOpen);
     toggle.style.transform = isOpen ? "rotate(0deg)" : "rotate(90deg)";
 });
