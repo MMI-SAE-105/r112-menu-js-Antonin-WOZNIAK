70e47f1c75e22f9538c69cbba7277d6b21be864c
Antonin-WOZNIAK <antonin.wozniak@edu.univ-fcomte.fr>
2024-11-15 16:30:42 +0100

    ouverture/fermeture menu.

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,8 @@ const toggle = document.querySelector("button.menu-btn");
 const nav = document.querySelector("nav.menu");
 toggle.addEventListener("click", () => {   
     const isOpen = toggle.ariaExpanded === "true";
+    const isClosed = !isOpen;
+    console.log("isOpen : ", isOpen, "isClosed : ", isClosed);
     nav.ariaHidden = isOpen ? "true" : "false";
     toggle.ariaExpanded = isOpen ? "false" : "true";
     toggle.style.transform = isOpen ? "rotate(0deg)" : "rotate(90deg)";
