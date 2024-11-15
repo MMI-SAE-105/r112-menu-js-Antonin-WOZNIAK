a4d438254b9e4b93571a4ae61f38b4a7efb561a4
Antonin-WOZNIAK <antonin.wozniak@edu.univ-fcomte.fr>
2024-11-15 16:27:22 +0100

    ouverture menu

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+const toggle = document.querySelector("button.menu-btn");
+const nav = document.querySelector("nav.menu");
+toggle.addEventListener("click", () => {   
+    const isOpen = toggle.ariaExpanded === "true";
+    nav.ariaHidden = isOpen ? "true" : "false";
+    toggle.ariaExpanded = isOpen ? "false" : "true";
+    toggle.style.transform = isOpen ? "rotate(0deg)" : "rotate(90deg)";
+});
